408710fd933d7ba2f56e92681c100213e10c8890
AmeyaGosavi <89070847+AmeyaGosavi@users.noreply.github.com>
2021-09-11 01:23:00 -0700

    Add files via upload

---
+++ react_api_project/src/Shared/UI/Buttons/Edit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Edit = (props) => {
+    return (
+        <button className="btn btn-success">
+            {props.buttonName}
+        </button>
+    )
+}
+
+export default Edit
