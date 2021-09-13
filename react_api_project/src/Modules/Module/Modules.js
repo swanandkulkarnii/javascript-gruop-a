65223fbbb2d0ce511dfc887d9f45f6bcf25b1ed8
AmeyaGosavi <89070847+AmeyaGosavi@users.noreply.github.com>
2021-09-12 20:07:08 -0700

    Add files via upload

--- react_api_project/src/Modules/Module/Modules.js
+++ react_api_project/src/Modules/Module/Modules.js
@@ -1,69 +1,92 @@
 import React, { useState } from 'react'
-import Input from '../../Shared/UI/Input/Input';
+import PopupModal from '../../Shared/UI/PopupModal/PopupModal';
 import ModuleService from './ModuleService';
+import ModuleForm from './ModuleForm';
+import Input from '../../Shared/UI/Input/Input';
+import Td from '../../Shared/UI/Table/Td';
 const Modules = () => {
-    const database = JSON.parse(localStorage.getItem('Proj_Module_Api'));
-    const [projectId, setProjectId] = useState();
-    const [moduleTitle, setModuleTitle] = useState();
-    const [moduleDesc,setModuleDesc] = useState('');
+    const [buttonPopup, setButtonPopup] = useState(false);
 
-    const projectVal = (event) =>{
-        setProjectId(event.target.value);
-    }
-    const moduleTitleHandler = (event) =>{
-        setModuleTitle(event.target.value);
-    }
-    const  moduleDescHandler = (event) =>{
-        setModuleDesc(event.target.value);
-    }
-    const submitModuleHandler = (event) =>{
-        event.preventDefault();
-        if(projectId!=='' && moduleTitle!=='' && moduleDesc!=='')
-        {
-            const proj_module = {moduleTitle: moduleTitle, moduleDesc: moduleDesc};
-            for(var i = 0;i<database.length;i++)
+    const [searchModuleTitle, setSearchModuleTitle] = useState('');
+    const [searchResult, setSearchResult] = useState('');
+    
+    const searchModuleTitleHandler = (event) =>{
+        setSearchModuleTitle(event.target.value);
+        const project_data = JSON.parse(localStorage.getItem('Proj_Module_Api'));
+	    for(var i=0; i<project_data.length;i++)
+	    {
+		    for(var j = 0; j<project_data[i].modules.length;j++)
 		    {
-			if(database[i].pid === Number(projectId))
-			{
-				if(database[i].modules === undefined)
-				{
-                    console.log('SAD')
-					let modules_list = new Array();
-					proj_module.mid = 0;
-					modules_list.push(proj_module);
-					database[i].modules = modules_list;
-					localStorage.setItem("Proj_Module_Api", JSON.stringify(database));
-				}	
-				else
-				{
-					for(var j=0; j<database[i].modules.length;j++)
-					{
-		 				var id = database[i].modules[j].mid++;
-					}
-					proj_module.mid = id;
-					database[i].modules.push(proj_module);
-					localStorage.setItem("Proj_Module_Api",JSON.stringify(database));
-				}
-                setProjectId('');
-                    setModuleTitle('');
-                    setModuleDesc('');
-			}
-		    }
-            /*database.map((value,index)=>{
-                if(value.pid === Number(projectId))
-                {
-                    if(value.modules === undefined)
-                    {
-                        const module = {mid:Math.floor(Math.random()*100000+1),moduleTitle: moduleTitle, moduleDesc:moduleDesc};
-                        console.log(module);
-                    }
-                }
-            })*/
+			    if(project_data[i].modules[j].moduleTitle.toUpperCase().includes(searchModuleTitle.toUpperCase()))
+			    {
+                    setSearchResult(
+                    <tr>
+                        <Td data = {`${JSON.stringify(project_data[i].modules[j].moduleTitle)}`}></Td>
+                        <Td data = {`${JSON.stringify(project_data[i].modules[j].moduleDesc)}`}></Td>
+                    </tr>)
+                    console.log(`${JSON.stringify(project_data[i].modules[j].moduleTitle)}`);
+			    }
+		    }	
+	    }
+    };
+
+    var removeByAttr = function(arr, attr, value){
+        var i = arr.length;
+        while(i--){
+           if( arr[i] 
+               && arr[i].hasOwnProperty(attr) 
+               && (arguments.length > 2 && arr[i][attr] === value ) ){ 
+    
+               arr.splice(i,1);
+           }
         }
+        return arr;
+    }
+
+    const deleteModuleHandler = (mid,pid) =>{
+        console.log(mid,pid);
+        var project_data = JSON.parse(localStorage.getItem("Proj_Module_Api"));
+		for(var i=0; i<project_data.length;i++)
+		{
+			if(project_data[i].pid === pid)
+			{
+				project_data[i].modules = removeByAttr(project_data[i].modules,"mid",mid);
+			}	
+		}
+		localStorage.setItem('Proj_Module_Api',JSON.stringify(project_data));
+        window.location.reload(true);
+    }
+    const editModuleHandler = (mid,pid) =>{
+        console.log(mid,pid);
     }
     return (
         <div className="container">
             <h1 className="text-center"> Add New Modules</h1>
+            <table>
+                <thead>
+                    <th>Module Title</th>
+                    <th>Module Description</th>
+                </thead>
+                {searchResult}
+            </table>
+            <Input 
+                label="Search" 
+                input={{
+                    id : 'search_module',
+                    type : 'search',
+                    placeholder:'Enter Module Title',
+                    name:"searchModule"
+                }}
+                value = {searchModuleTitle}
+                onChange={searchModuleTitleHandler}
+            ></Input>
+            <button
+                type="button"
+                className="btn btn-primary my-5"
+                onClick={() => setButtonPopup(true)}
+            >
+                Add New Module
+            </button>
             <table className="table">
                 <thead>
                     <th>Module Title</th>
@@ -71,47 +94,15 @@ const Modules = () => {
                     <th>Edit</th>
                     <th>Delete</th>
                 </thead>
-                {<ModuleService></ModuleService>}
+                {<ModuleService onModuleDelete = {deleteModuleHandler} onModuleEdit = {editModuleHandler}></ModuleService>}
             </table>
-            <form onSubmit={submitModuleHandler}>
-                <div className="form-group">
-                    <select className="form-control" onChange={projectVal} value={projectId}>
-                        <option>Select Project</option>
-                        {
-                            database.map((value, index)=>{
-                                return(
-                                    <option value={value.pid}>{value.projTitle}</option>
-                                );
-                            })
-                        }
-                    </select>    
-                </div>
-                <Input 
-                    label="Module Title" 
-                    input={{
-                        id : 'module_title',
-                        type : 'text',
-                        placeholder:'Enter Module Title',
-                        name:"moduleTitle"
-                    }}
-                    value = {moduleTitle}
-                    onChange={moduleTitleHandler}
-                >
-                </Input>
-                <Input 
-                    label="Module Description" 
-                    input={{
-                        id : 'module_ddesc',
-                        type : 'text',
-                        placeholder:'Enter Module Description',
-                        name:"moduleDesc"
-                    }}
-                    value = {moduleDesc}
-                    onChange={moduleDescHandler}
-                >
-                </Input>
-                <button className="btn btn-success mt-5">Add Module</button>
-            </form>
+            <PopupModal
+                trigger={buttonPopup}
+                settrigger={setButtonPopup}
+                title = "Add Module"
+            >
+                <ModuleForm></ModuleForm>
+            </PopupModal>
         </div>
     )
 }
