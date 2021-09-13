23f90f19db4647c9385454a06616514d57b06165
AmeyaGosavi <89070847+AmeyaGosavi@users.noreply.github.com>
2021-09-12 20:05:33 -0700

    Add files via upload

--- ReactTeamProject/src/Modules/Module/ModuleService.js
+++ ReactTeamProject/src/Modules/Module/ModuleService.js
@@ -1,33 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Td from '../../Shared/UI/Table/Td';
 import Edit from '../../Shared/UI/Buttons/Edit';
 import Delete from '../../Shared/UI/Buttons/Delete';
+import PopupModal from '../../Shared/UI/PopupModal/PopupModal';
+import ModuleForm from './ModuleForm';
 const ModuleService = (props) => {
     const database = JSON.parse(localStorage.getItem('Proj_Module_Api'));
-    
-    const moduleD = database.map((value, index)=>{
-            return value.modules;
-    });
-    console.log(moduleD);
+
+    const [buttonPopup, setButtonPopup] = useState(false);
     if(database!==null){
         return (
-            <tbody >
-            {
-                moduleD.map((moduleData, index)=>{
-                    return(
-                        moduleD[index].map((mdouleObj)=>{
-                            return(
-                            <tr key={index}>
-                                <Td data = {mdouleObj.moduleTitle}></Td>
-                                <Td data = {mdouleObj.moduleDesc}></Td>
-                                <Td>{mdouleObj.mid}</Td>
-                                <Td><Edit other = {{onClick:() => {props.onModuleEdit(mdouleObj.mid)}}} buttonName = "Edit"></Edit></Td>
-                                <Td><Delete other = {{onClick:() => {props.onModuleDelete(mdouleObj.mid)}}} buttonName = "Delete"></Delete></Td>
-                            </tr>)
-                        })
-                    );
-                })
-            }
+            <tbody>
+                {
+                    database.map((value,index)=>{
+                        return(
+                            value.modules.map((moduleData,index)=>{
+                                return(
+                                    <tr key={index}>
+                                        <Td data = {moduleData.moduleTitle}></Td>
+                                        <Td data = {moduleData.moduleDesc}></Td>
+                                        <Td><Edit other = {{onClick:() => {props.onModuleEdit(moduleData.mid,value.pid)},onClick:() => {setButtonPopup(true)}}}  buttonName = "Edit"></Edit></Td>
+                                        <Td><Delete other = {{onClick:() => {props.onModuleDelete(moduleData.mid,value.pid)}}} buttonName = "Delete"></Delete></Td>
+                                    </tr>
+                                )
+                            })
+                        )
+                    })
+                }
+                <PopupModal
+                    trigger={buttonPopup}
+                    settrigger={setButtonPopup}
+                    title = "Add Module"
+                >
+                    <ModuleForm></ModuleForm>
+                </PopupModal>                            
             </tbody>
         )
     }
--- ReactTeamProject/src/Modules/Module/Modules.js
+++ ReactTeamProject/src/Modules/Module/Modules.js
@@ -2,22 +2,87 @@ import React, { useState } from 'react'
 import PopupModal from '../../Shared/UI/PopupModal/PopupModal';
 import ModuleService from './ModuleService';
 import ModuleForm from './ModuleForm';
-
+import Input from '../../Shared/UI/Input/Input';
+import Td from '../../Shared/UI/Table/Td';
 const Modules = () => {
     const [buttonPopup, setButtonPopup] = useState(false);
+
+    const [searchModuleTitle, setSearchModuleTitle] = useState('');
+    const [searchResult, setSearchResult] = useState('');
     
-    const deleteModuleHandler = (mid) =>{
-        console.log(mid);
+    const searchModuleTitleHandler = (event) =>{
+        setSearchModuleTitle(event.target.value);
+        const project_data = JSON.parse(localStorage.getItem('Proj_Module_Api'));
+	    for(var i=0; i<project_data.length;i++)
+	    {
+		    for(var j = 0; j<project_data[i].modules.length;j++)
+		    {
+			    if(project_data[i].modules[j].moduleTitle.toUpperCase().includes(searchModuleTitle.toUpperCase()))
+			    {
+                    setSearchResult(
+                    <tr>
+                        <Td data = {`${JSON.stringify(project_data[i].modules[j].moduleTitle)}`}></Td>
+                        <Td data = {`${JSON.stringify(project_data[i].modules[j].moduleDesc)}`}></Td>
+                    </tr>)
+                    console.log(`${JSON.stringify(project_data[i].modules[j].moduleTitle)}`);
+			    }
+		    }	
+	    }
+    };
+
+    var removeByAttr = function(arr, attr, value){
+        var i = arr.length;
+        while(i--){
+           if( arr[i] 
+               && arr[i].hasOwnProperty(attr) 
+               && (arguments.length > 2 && arr[i][attr] === value ) ){ 
+    
+               arr.splice(i,1);
+           }
+        }
+        return arr;
     }
-    const editModuleHandler = (mid) =>{
-        console.log(mid);
+
+    const deleteModuleHandler = (mid,pid) =>{
+        console.log(mid,pid);
+        var project_data = JSON.parse(localStorage.getItem("Proj_Module_Api"));
+		for(var i=0; i<project_data.length;i++)
+		{
+			if(project_data[i].pid === pid)
+			{
+				project_data[i].modules = removeByAttr(project_data[i].modules,"mid",mid);
+			}	
+		}
+		localStorage.setItem('Proj_Module_Api',JSON.stringify(project_data));
+        window.location.reload(true);
+    }
+    const editModuleHandler = (mid,pid) =>{
+        console.log(mid,pid);
     }
     return (
         <div className="container">
             <h1 className="text-center"> Add New Modules</h1>
+            <table>
+                <thead>
+                    <th>Module Title</th>
+                    <th>Module Description</th>
+                </thead>
+                {searchResult}
+            </table>
+            <Input 
+                label="Search" 
+                input={{
+                    id : 'search_module',
+                    type : 'search',
+                    placeholder:'Enter Module Title',
+                    name:"searchModule"
+                }}
+                value = {searchModuleTitle}
+                onChange={searchModuleTitleHandler}
+            ></Input>
             <button
                 type="button"
-                className="btn btn-primary"
+                className="btn btn-primary my-5"
                 onClick={() => setButtonPopup(true)}
             >
                 Add New Module
@@ -34,6 +99,7 @@ const Modules = () => {
             <PopupModal
                 trigger={buttonPopup}
                 settrigger={setButtonPopup}
+                title = "Add Module"
             >
                 <ModuleForm></ModuleForm>
             </PopupModal>
