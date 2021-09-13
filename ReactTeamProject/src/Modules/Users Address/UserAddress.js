3079292969b622c7edd68a5094ad6d616fe5cc5a
Krishna-Ramchandani <89068822+Krishna-Ramchandani@users.noreply.github.com>
2021-09-13 12:59:26 +0530

    Add files via upload

--- ReactTeamProject/src/Modules/Users Address/UserAddress.js
+++ ReactTeamProject/src/Modules/Users Address/UserAddress.js
@@ -1,11 +1,127 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import UserAddressForm from "./UserAddressForm";
+import UserAddressService from "./UserAddressService";
+import PopupModal from "../../Shared/UI/PopupModal/PopupModal";
+import Input from "../../Shared/UI/Input/Input";
+import Td from "../../Shared/UI/Table/Td";
 
-const UserAddress = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const UserAddress = (props) => {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResult, setSearchResult] = useState("");
 
-export default UserAddress
+  useEffect(() => {
+    if (localStorage.getItem("UserAddress") == null) {
+      localStorage.setItem("UserAddress", JSON.stringify([]));
+    }
+  }, []);
+
+  const searchUserAddressHandler = (event) => {
+    setSearchValue(event.target.value);
+    var search_data = JSON.parse(localStorage.getItem("UserAddress"));
+    for (var i = 0; i < search_data.length; i++) {
+      if (
+        search_data[i].city
+          .toUpperCase()
+          .includes(searchValue.toLocaleUpperCase())
+      ) {
+        setSearchResult(
+          <tr>
+            <Td data={`${JSON.stringify(search_data[i].addressOne)}`}></Td>
+            <Td data={`${JSON.stringify(search_data[i].addressTwo)}`}></Td>
+            <Td data={`${JSON.stringify(search_data[i].city)}`}></Td>
+            <Td data={`${JSON.stringify(search_data[i].state)}`}></Td>
+            <Td data={`${JSON.stringify(search_data[i].pincode)}`}></Td>
+            <Td data={`${JSON.stringify(search_data[i].country)}`}></Td>
+          </tr>
+        );
+      }
+    }
+  };
+
+  var removeByAttr = function (arr, attr, value) {
+    var i = arr.length;
+    while (i--) {
+      console.log(arr+attr+value);
+      console.log("While Executed!!!");
+      if (
+        arr[i] &&
+        arr[i].hasOwnProperty(attr) &&
+        (arguments.length > 2 && arr[i][attr] === value)
+      ) {
+        console.log("If Executed!!!");
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
+  };
+  const deleteHandler = (uid) => {
+    var address_data = JSON.parse(localStorage.getItem("UserAddress"));
+    address_data = removeByAttr(address_data, "uid", uid);
+    localStorage.setItem("UserAddress", JSON.stringify(address_data));
+    console.log("Executed!!!");
+    window.location.reload(true);
+  };
+  const editHandler = (uid) => {};
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-center">Add New User Address</h1>
+        <table className="table">
+          <thead>
+            <th>Address Line One</th>
+            <th>Address Line Two</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Pincode</th>
+            <th>Country</th>
+          </thead>
+          {searchResult}
+        </table>
+        <Input
+          label="Search User Address"
+          input={{
+            id: "search_address",
+            type: "search",
+            placeholder: "Enter City to Search",
+            name: "searchUserAddress",
+          }}
+          value={searchValue}
+          onChange={searchUserAddressHandler}
+        />
+
+        <br />
+        <br />
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => setButtonPopup(true)}
+        >
+          Add New User Address
+        </button>
+        <table className="table">
+          <thead>
+            <th>Address Line One</th>
+            <th>Address Line Two</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Pincode</th>
+            <th>Country</th>
+          </thead>
+          {
+            <UserAddressService
+              onEdit={editHandler}
+              onDelete={deleteHandler}
+            ></UserAddressService>
+          }
+        </table>
+        <PopupModal trigger={buttonPopup} settrigger={setButtonPopup}>
+          <UserAddressForm />
+        </PopupModal>
+      </div>
+    </>
+  );
+};
+
+export default UserAddress;
---
+++ ReactTeamProject/src/Modules/Users Address/UserAddressForm.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import Input from "../../Shared/UI/Input/Input";
+
+
+const UserAddressForm = () => {
+  const [addressOne, setAddressOne] = useState("");
+  const [addressTwo, setAddressTwo] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [pincode, setPincode] = useState("");
+  const [country, setCountry] = useState("");
+
+
+  const addressOneHandler = (event) => {
+    setAddressOne(event.target.value);
+  };
+  const addressTwoHandler = (event) => {
+    setAddressTwo(event.target.value);
+  };
+  const cityHandler = (event) => {
+    setCity(event.target.value);
+  };
+  const stateHandler = (event) => {
+    setState(event.target.value);
+  };
+  const pincodeHandler = (event) => {
+    setPincode(event.target.value);
+  };
+  const countryHandler = (event) => {
+    setCountry(event.target.value);
+  };
+
+  const submitUserAddressHandler = (event) => {
+    event.preventDefault();
+    if (
+      addressOne !== "" &&
+      addressTwo !== "" &&
+      city !== "" &&
+      state !== "" &&
+      pincode !== "" &&
+      country !== ""
+    ) {
+      const useraddress = {
+        uid: Math.floor(Math.random() * 100000 + 1),
+        addressOne: addressOne,
+        addressTwo: addressTwo,
+        city: city,
+        state: state,
+        pincode: pincode,
+        country: country,
+        isDeleted: false,
+      };
+      const db = JSON.parse(localStorage.getItem("UserAddress"));
+      db.push(useraddress);
+      localStorage.setItem("UserAddress", JSON.stringify(db));
+      setAddressOne("");
+      setAddressTwo("");
+      setCity("");
+      setState("");
+      setPincode("");
+      setCountry("");
+    } else {
+      alert("Please enter all Address Data");
+    }
+  };
+   
+  return (
+    <form className="form-horizontal form-popup" id="UserAddressForm">
+      <Input
+        label="Address Line One"
+        input={{
+          id: "address_line_one",
+          type: "text",
+          placeholder: "Enter User Address Line One",
+          name: "useraddressone",
+        }}
+        value={addressOne}
+        onChange={addressOneHandler}
+      />
+      <Input
+        label="Address Line Two"
+        input={{
+          id: "address_Line_two",
+          type: "text",
+          placeholder: "Enter User Address Line Two",
+          name: "useraddresstwo",
+        }}
+        value={addressTwo}
+        onChange={addressTwoHandler}
+      />
+      <Input
+        label="User City"
+        input={{
+          id: "user_city",
+          type: "text",
+          placeholder: "Enter User City",
+          name: "usercity",
+        }}
+        value={city}
+        onChange={cityHandler}
+      />
+      <Input
+        label="User State"
+        input={{
+          id: "user_state",
+          type: "text",
+          placeholder: "Enter User State",
+          name: "userstate",
+        }}
+        value={state}
+        onChange={stateHandler}
+      />
+      <Input
+        label="User Pincode"
+        input={{
+          id: "user_pincode",
+          type: "number",
+          placeholder: "Enter User Pincode",
+          name: "userpincode",
+        }}
+        value={pincode}
+        onChange={pincodeHandler}
+      />
+      <Input
+        label="User Country"
+        input={{
+          id: "address_one",
+          type: "text",
+          placeholder: "Enter User Country",
+          name: "usercountry",
+        }}
+        value={country}
+        onChange={countryHandler}
+      />
+      <button
+        className="btn btn-success mt-5"
+        onClick={submitUserAddressHandler}
+      >
+        Add User Address
+      </button>
+      
+    </form>
+  );
+};
+
+export default UserAddressForm;
