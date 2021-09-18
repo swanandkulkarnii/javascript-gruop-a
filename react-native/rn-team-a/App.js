64e0b7bc51bf28f87b8e767c90c34bdb17165d3e
Krishna-Ramchandani <89068822+Krishna-Ramchandani@users.noreply.github.com>
2021-09-18 23:30:06 +0530

    Updated App.js file. Added Navigations.

--- react-native/rn-team-a/App.js
+++ react-native/rn-team-a/App.js
@@ -3,6 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 import Header from "./components/Header";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Colors from "./constants/color";
+
+import AppHomeScreen from "./screens/AppHomeScreen";
+import ProjectScreen from "./screens/ProjectScreen";
+import ModuleScreen from "./screens/ModuleScreen";
+import ApiScreen from "./screens/ApiScreen";
+import UserScreen from "./screens/UserScreen";
+import UserAddressScreen from "./screens/UserAddressScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -24,11 +34,26 @@ export default function App() {
     );
   }
 
+  const Drawer = createDrawerNavigator();
+
   return (
-    <View style={styles.screen}>
-      <Header title="App.js File / Module API Project" />
-      <Text style={styles.textTitle}>Welcome to BITS!</Text>
-    </View>
+    
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: Colors.header },
+         
+          fontWeight: "bold",
+        }}
+      >
+        <Drawer.Screen name="Home" component={AppHomeScreen} />
+        <Drawer.Screen name="Project" component={ProjectScreen} />
+        <Drawer.Screen name="API" component={ApiScreen} />
+        <Drawer.Screen name="Modules" component={ModuleScreen} />
+        <Drawer.Screen name="Users" component={UserScreen} />
+        <Drawer.Screen name="User Address" component={UserAddressScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -41,5 +66,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginVertical: 10,
     fontFamily: "open-sans-bold",
+  },
+  headerColor: {
+    backgroundColor: Colors.header,
   },
 });
