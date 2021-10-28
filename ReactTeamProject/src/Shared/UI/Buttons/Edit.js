d3191bc43a58af4660123bc95b7999e32b0c6927
AmeyaGosavi <89070847+AmeyaGosavi@users.noreply.github.com>
2021-10-28 10:26:58 +0530

    Add files via upload

--- ReactTeamProject/src/Shared/UI/Buttons/Edit.js
+++ ReactTeamProject/src/Shared/UI/Buttons/Edit.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import { AiTwotoneEdit } from "react-icons/ai";
 const Edit = (props) => {
     return (
         <button className="btn btn-success" {...props.other}>
+            <AiTwotoneEdit /> &nbsp;
             {props.buttonName}
         </button>
     )
