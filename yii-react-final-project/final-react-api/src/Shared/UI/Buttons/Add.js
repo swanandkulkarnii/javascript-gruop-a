89e71675dfbca844c0e18d4ce845f02034fc9733
swanandkulkarnii <89059982+swanandkulkarnii@users.noreply.github.com>
2021-10-28 11:48:24 +0530

    Add files via upload

--- yii-react-final-project/final-react-api/src/Shared/UI/Buttons/Add.js
+++ yii-react-final-project/final-react-api/src/Shared/UI/Buttons/Add.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { AiFillPlusSquare } from "react-icons/ai";
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 const Add = (props) => {
-    return (
-        <button className="btn btn-success my-5" {...props.other}>
-            <AiFillPlusSquare /> &nbsp;
-            {props.buttonName}
-        </button>
-    )
-}
+  return (
+    <button className="btn btn-success my-5" {...props.other}>
+      <AiOutlinePlus /> &nbsp;
+      {props.buttonName}
+    </button>
+  );
+};
 
-export default Add
+export default Add;
