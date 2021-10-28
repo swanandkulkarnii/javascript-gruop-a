b8018c9144d8f84635a718b2c38c1e82c532c41d
husainrounak <89070068+husainrounak@users.noreply.github.com>
2021-10-28 14:36:14 +0530

    Update UsersController.php

--- yii-react-final-project/final-yii-rest-api/controllers/UsersController.php
+++ yii-react-final-project/final-yii-rest-api/controllers/UsersController.php
@@ -4,4 +4,23 @@ use yii\rest\ActiveController;
 class UsersController extends BaseController
 {
     public $modelClass = 'app\resource\UsersResource';
+    
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        $actions['index']['dataFilter'] = [
+            'class' => \yii\data\ActiveDataFilter::class,
+            'searchModel' => $this->modelClass,
+        ];
+        return $actions;
+    }
+
+    public function actionRead() {
+        $activeData = new ActiveDataProvider([
+            'query' => \app\models\Users::find()->where(['is_deleted'=> 0]),
+        ]);
+        return $activeData;
+       
+    }
 }
