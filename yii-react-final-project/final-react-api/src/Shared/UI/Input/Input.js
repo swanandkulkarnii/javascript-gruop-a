31348ae4ba0029a3497b8d8b3a6b2346a2911b56
swanandkulkarnii <89059982+swanandkulkarnii@users.noreply.github.com>
2021-10-29 14:26:48 +0530

    Add files via upload

--- yii-react-final-project/final-react-api/src/Shared/UI/Input/Input.js
+++ yii-react-final-project/final-react-api/src/Shared/UI/Input/Input.js
@@ -2,7 +2,9 @@ import React from "react";
 const Input = React.forwardRef((props) => {
   return (
     <div className="form-group mt-3">
-      <label htmlFor={props.input.id}>{props.label}</label>
+      <b>
+        <label htmlFor={props.input.id}>{props.label}</label>
+      </b>
       <input
         {...props.input}
         className="form-control"
